e24dd97b1ca3ae942c630c43b7fb76a23d2cccd9
tlparche <h010508@naver.com>
2023-06-05 20:15:02 +0900

    complete new js

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,15 +1,29 @@
 import Layout from '@/components/Layout'
+import {useState} from 'react'
+import axios from 'axios'
 export default function NewProduct(){
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    function createProduct(){
+        const data = {title, description, price};
+        axios.post("/api/products", data);
+    }
     return(
         <Layout>
-            <h1>New Product</h1>
-            <label>Product name</label>
-            <input type="text" placeholder={"product name"}/>
-            <label>Description</label>
-            <textarea placeholder={"description"} />
-            <label>Price (in USD)</label>
-            <input type="text" placeholder={"price"}/>
-            <button className="btn-primary">Save</button>
+            <form onSubmit={createProduct}>
+                <h1>New Product</h1>
+                <label>Product name</label>
+                <input type="text" placeholder={"product name"} value={title}
+                       onChange={event => setTitle(event.target.value)}/>
+                <label>Description</label>
+                <textarea placeholder={"description"} value={description}
+                          onChange={event => setDescription(event.target.value)}/>
+                <label>Price (in USD)</label>
+                <input type="text" placeholder={"price"} value={price}
+                       onChange={event => setPrice(event.target.value)}/>
+                <button type={'submit'} className="btn-primary">Save</button>
+            </form>
         </Layout>
     )
 }
